fd997fe3177b10f4bed8a6d32a401450bc7996a5
Corentin Friant <corentin.friant@gmail.com>
2023-01-06 00:50:03 +0100

    Ajout de la modification d'un bien

--- src/Controller/Admin/BiensController.php
+++ src/Controller/Admin/BiensController.php
@@ -48,14 +48,39 @@ class BiensController extends AbstractController
 
         return $this->render('admin/biens/ajout.html.twig',compact('biensForm'));
     }
+
     #[Route ('/modifier/{id}',name:'modifier')]
-    public function modifier(Biens $biens): Response
-    {
-        return $this->render('admin/biens/index.html.twig');
+    public function modifier(BiensImmobiliers $biens,Request $request,EntityManagerInterface $emi,SluggerInterface $slugger): Response
+    { 
+        // on crée le formulaire
+        $biensForm = $this->createForm(BiensFormType::class, $biens);
+
+        // on traite la requête du formulaire
+        $biensForm ->handleRequest($request);
+        
+        // on vérifie si le formulaire est soumis et valide
+        if ($biensForm->isSubmitted() && $biensForm->isValid()) { 
+            // on génère le slug
+            $slug = $slugger -> slug($biens->getTitre());
+            $biens->setSlug($slug);
+
+               // On stocke
+               $emi->persist($biens);
+               $emi->flush();
+
+               $this->addFlash('success', 'Bien ajouté avec succès');
+
+               return $this->redirectToRoute('admin_biens_index');
+        }
+
+        return $this->render('admin/biens/modifier.html.twig',compact('biensForm'));
     }
+
+
     #[Route ('/supprimer/{id}',name:'supprimer')]
     public function supprimer(Biens $biens): Response
-    {
+    {   
+       
         return $this->render('admin/biens/index.html.twig');
     }
 }
