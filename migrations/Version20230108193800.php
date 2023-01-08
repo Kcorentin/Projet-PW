684d258dabfd083590a8e466b5fa23232295a4c4
Corentin Friant <corentin.friant@gmail.com>
2023-01-08 22:46:05 +0100

    ajout première page

--- migrations/Version20230108193800.php
+++ migrations/Version20230108193800.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230104221423 extends AbstractMigration
+final class Version20230108193800 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,16 +20,12 @@ final class Version20230104221423 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE biens_immobiliers ADD slug VARCHAR(255) NOT NULL');
-        $this->addSql('ALTER TABLE biens_recherche ADD slug VARCHAR(255) NOT NULL');
-        $this->addSql('ALTER TABLE categories ADD slug VARCHAR(255) NOT NULL');
+
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE biens_immobiliers DROP slug');
-        $this->addSql('ALTER TABLE biens_recherche DROP slug');
-        $this->addSql('ALTER TABLE categories DROP slug');
+
     }
 }
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,15 +2,41 @@
 
 namespace App\Controller;
 
+
+use App\Controller\EntityManager;
+use App\Entity\BiensImmobiliers;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\BiensImmobiliersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class MainController extends AbstractController
 {
     #[Route('/', name: 'main')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
-        return $this->render('main/index.html.twig');
+     
+
+
+        $biensRepository = $doctrine->getRepository(BiensImmobiliers::class);
+        $query = $biensRepository->createQueryBuilder('b')
+        ->getQuery();
+        $randombien = $query->getResult();
+            
+    
+       
+        $random1= rand(0,count($randombien)-1);
+        $random2= rand(0,count($randombien)-1);
+        $random3= rand(0,count($randombien)-1);
+        
+
+
+        return $this->render('main/index.html.twig',[
+           'biens'=>$randombien, 'r1'=>$random1,
+           'r2'=>$random2,'r3'=>$random3,
+        ]);
     }
+ 
 }
